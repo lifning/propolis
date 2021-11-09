17d542022c4a1e91913c8ec82a317e3ee63641d9
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2021-11-09 18:46:57 +0000

    first cut at request handler
    only printing messages received atm

--- propolis/src/hw/virtio/p9fs.rs
+++ propolis/src/hw/virtio/p9fs.rs
@@ -8,10 +8,11 @@ use crate::util::regmap::RegMap;
 
 use super::bits::*;
 use super::pci::{PciVirtio, PciVirtioState};
-use super::queue::{VirtQueue, VirtQueues};
+use super::queue::{Chain, VirtQueue, VirtQueues};
 use super::VirtioDevice;
 
 use lazy_static::lazy_static;
+use rs9p::{Msg, serialize::read_msg};
 
 pub struct PciVirtio9pfs {
     virtio_state: PciVirtioState,
@@ -22,6 +23,7 @@ pub struct PciVirtio9pfs {
 }
 
 impl PciVirtio9pfs {
+
     pub fn new(source: String, target: String, queue_size: u16) -> Arc<Self> {
         let queues = VirtQueues::new(
             NonZeroU16::new(queue_size).unwrap(),
@@ -37,6 +39,51 @@ impl PciVirtio9pfs {
         );
         Arc::new(Self{virtio_state, pci_state, source, target})
     }
+
+    pub fn handle_req(&self, vq: &Arc<VirtQueue>, ctx: &DispCtx) -> Option<Msg> {
+        let mem = &ctx.mctx.memctx();
+
+        let mut chain = Chain::with_capacity(1);
+        let clen = vq.pop_avail(&mut chain, mem)? as usize;
+
+        //TODO better as uninitialized?
+        let mut data: Vec<u8> = vec![0;clen];
+        let buf = data.as_mut_slice();
+        
+        // TODO copy pasta from tail end of Chain::read function. Seemingly
+        // cannot use Chain::read as-is because it expects a statically sized
+        // type.
+        let mut done = 0;
+        let total = chain.for_remaining_type(true, |addr, len| {
+            let remain = &mut buf[done..];
+            if let Some(copied) = mem.read_into(addr, remain, len) {
+                let need_more = copied != remain.len();
+                done += copied;
+                (copied, need_more)
+            } else {
+                (0, false)
+            }
+        });
+
+        if total != clen {
+            //TODO error msg
+            return None
+        }
+
+        let mut rdr = std::io::Cursor::new(data);
+        match read_msg(&mut rdr) {
+            Ok(msg) => {
+                println!("ok: ← {:#?}", msg);
+                Some(msg)
+            }
+            Err(_) => {
+                //TODO error msg
+                println!("err: ← {:?}", rdr.get_ref());
+                None
+            }
+        }
+    }
+
 }
 
 impl VirtioDevice for PciVirtio9pfs {
@@ -60,8 +107,8 @@ impl VirtioDevice for PciVirtio9pfs {
 
     fn set_features(&self, _feat: u32) { }
 
-    fn queue_notify(&self, _vq: &Arc<VirtQueue>, _ctx: &DispCtx) {
-        unimplemented!();
+    fn queue_notify(&self, vq: &Arc<VirtQueue>, ctx: &DispCtx) {
+        self.handle_req(vq, ctx);
     }
 }
 
