c01c7c73d6fa5ff20a4118a3f992d2a57dce98bc
lif <>
2025-07-17 18:03:04 +0000

    smoothing out (and testing) report descriptor authoring

--- lib/propolis/src/hw/usb/usbdev/endpoint/interrupt.rs
+++ lib/propolis/src/hw/usb/usbdev/endpoint/interrupt.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use crate::hw::usb::usbdev::requests::{RequestType, SetupData};
+use crate::hw::usb::usbdev::requests::SetupData;
 
 pub enum InInterruptRequestInfo {}
 
