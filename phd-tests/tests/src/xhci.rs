b4a4fc937ed71b9648e7f47c56b271c608b7e483
lif <>
2026-02-18 06:49:32 +0000

    start writing migration-while-usb-device-in-use test

--- phd-tests/tests/src/xhci.rs
+++ phd-tests/tests/src/xhci.rs
@@ -10,10 +10,11 @@ use std::{
     time::Duration,
 };
 
-use phd_testcase::*;
+use phd_testcase::{phd_framework::test_vm::MigrationTimeout, *};
 use propolis_client::instance_spec::{
     PciPath, SpecKey, UsbDevice, UsbDeviceType, XhciController,
 };
+use uuid::Uuid;
 
 #[phd_testcase]
 async fn usb_device_enumerates(ctx: &TestCtx) {
@@ -29,7 +30,7 @@ async fn usb_device_enumerates(ctx: &TestCtx) {
         .usb_device(UsbDevice {
             xhc_device: xhc_name,
             root_hub_port_num: USB_PORT,
-            usb_device_type: UsbDeviceType::HidTablet,
+            usb_device_type: UsbDeviceType::Null,
         });
 
     let spec = config.vm_spec(ctx).await?;
@@ -104,4 +105,39 @@ async fn usb_tablet_vnc_pointer_events(ctx: &TestCtx) {
         output.contains("0000000 01"),
         "primary mouse button press (01) not found in raw HID event dump:\n{output}"
     );
+}
+
+#[phd_testcase]
+async fn usb_tablet_migration(ctx: &TestCtx) {
+    let mut config = ctx.vm_config_builder("usb_tablet_migration");
+    let xhc_name: SpecKey = "xhc0".into();
+    const PCI_DEV: u8 = 3;
+    const USB_PORT: u8 = 2;
+    config
+        .xhci_controller(
+            xhc_name.to_owned(),
+            XhciController { pci_path: PciPath::new(0, PCI_DEV, 0)? },
+        )
+        .usb_device(UsbDevice {
+            xhc_device: xhc_name,
+            root_hub_port_num: USB_PORT,
+            usb_device_type: UsbDeviceType::HidTablet,
+        });
+
+    let spec = config.vm_spec(ctx).await?;
+
+    let mut vm0 = ctx.spawn_vm_with_spec(spec, None).await?;
+    let mut vm1 = ctx
+        .spawn_successor_vm("usb_tablet_migration_successor", &vm0, None)
+        .await?;
+
+    vm0.launch().await?;
+    vm0.wait_to_boot().await?;
+
+    // TODO: start running hidraw read with i/o redirected, send click
+
+    vm1.migrate_from(&vm0, Uuid::new_v4(), MigrationTimeout::default()).await?;
+
+    // TODO: send different click, kill hidraw read, check contents of stdout file
+    todo!();
 }
